d4cff0a86c5ce46405f3c0dd03f9c49a7d620792
Rafael Pedrola <rafinskipg@gmail.com>
2016-07-29 12:54:28 +0200

    fix(git log): Ignores letter case
    Closes #54

--- tasks/lib/get-gitlog-commands.js
+++ tasks/lib/get-gitlog-commands.js
@@ -4,8 +4,8 @@ var debug = require('debug')('changelog:getGitLogCommands');
 
 function getGitLogCommands() {
   debug('getting log commands');
-  this.cmd.gitLog = 'git log --grep="%s" -E --format=%s %s..' + (this.options.branch ? this.options.branch : 'HEAD');
-  this.cmd.gitLogNoTag = 'git log ' + this.options.branch + ' --grep="%s" -E --format=%s';
+  this.cmd.gitLog = 'git log --grep="%s" -i -E --format=%s %s..' + (this.options.branch ? this.options.branch : 'HEAD');
+  this.cmd.gitLogNoTag = 'git log ' + this.options.branch + ' --grep="%s" -i -E --format=%s';
 }
 
 module.exports = getGitLogCommands;
