14da6fee8aa8b1fcec198ea26812aa1871008438
ycorneille <ycorneille>
2017-01-12 17:32:24 +0100

    feat(providers): Add possibility to set providers in the options

--- tasks/lib/get-provider-links.js
+++ tasks/lib/get-provider-links.js
@@ -3,33 +3,38 @@
 var debug = require('debug')('changelog:getProviderLinks');
 
 function getProviderLinks() {
-  debug('getting provider links');
-  // This is just in case they differ their urls at some point in the future.
-  // Also brings the posibility of adding more providers
-  var providerLinks = {
-    github: {
-      issue: '[#%s](' + this.options.repo_url + '/issues/%s)',
-      commit: '[%s](' + this.options.repo_url + '/commit/%s)'
-    },
-    bitbucket: {
-      issue: '[#%s](' + this.options.repo_url + '/issues/%s)',
-      commit: '[%s](' + this.options.repo_url + '/commits/%s)'
-    },
-    gitlab: {
-      issue: '[#%s](' + this.options.repo_url + '/issues/%s)',
-      commit: '[%s](' + this.options.repo_url + '/commit/%s)'
-    }
-  };
+    debug('getting provider links');
+    // This is just in case they differ their urls at some point in the future.
+    // Also brings the posibility of adding more providers
+    var providerLinks = {
+        github: {
+            issue: '[#%s](' + this.options.repo_url + '/issues/%s)',
+            commit: '[%s](' + this.options.repo_url + '/commit/%s)'
+        },
+        bitbucket: {
+            issue: '[#%s](' + this.options.repo_url + '/issues/%s)',
+            commit: '[%s](' + this.options.repo_url + '/commits/%s)'
+        },
+        gitlab: {
+            issue: '[#%s](' + this.options.repo_url + '/issues/%s)',
+            commit: '[%s](' + this.options.repo_url + '/commit/%s)'
+        }
+    };
 
-  if (this.options.repo_url.match(/bitbucket/)) {
-    this.provider = 'bitbucket';
-  } else if (this.options.repo_url.match(/gitlab/)) {
-    this.provider = 'gitlab';
-  } else {
-    // use github as default provider
-    this.provider = 'github';
-  }
-  this.links = providerLinks[this.provider];
+    if (this.options.provider && typeof(providerLinks[this.options.provider]) !== 'undefined') {
+        this.provider = this.options.provider;
+    }
+    else {
+        if (this.options.repo_url.match(/bitbucket/)) {
+            this.provider = 'bitbucket';
+        } else if (this.options.repo_url.match(/gitlab/)) {
+            this.provider = 'gitlab';
+        } else {
+            // use github as default provider
+            this.provider = 'github';
+        }
+    }
+    this.links = providerLinks[this.provider];
 }
 
 module.exports = getProviderLinks;
